6ce9061d2c608110bbe80af27a0ad15af96c5a13
Fatfara <thoufiqabubacker75@gmail.com>
2026-09-14 20:21:25 +0530

    Initial TuneFlow project setup

---
+++ tuneflow-backend/tuneflow-backend/src/main/java/com/tuneflow/TuneflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tuneflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneflowBackendApplication.class, args);
+	}
+
+}
---
+++ tuneflow-backend/tuneflow-backend/src/test/java/com/tuneflow/TuneflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuneflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
